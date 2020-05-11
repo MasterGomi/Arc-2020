6f51cb0efdb9dacedf2ba178b4d9b92387e9f954
Jackson Gray <jacksongray36@gmail.com>
2020-05-11 23:47:36 +1000

    Completed prototype for the paddles and a prototype for the kickbacks

--- Arc-2020/Assets/Scripts/PaddleControler.cs
+++ Arc-2020/Assets/Scripts/PaddleControler.cs
@@ -6,53 +6,43 @@ public class PaddleControler : MonoBehaviour
 {
     public bool IsLeft;
     public float MaxAngle;
-    public float AngleStep;
+    public float HitForce;
+    public float Damper;
 
-    private float _currentRotation = 0;
     private KeyCode _controlKey;
-    private int posMod = -1;
+    private JointSpring _spring;
+    private HingeJoint _joint;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (IsLeft)
+        _controlKey = IsLeft ? KeyCode.LeftArrow : KeyCode.RightArrow;
+        _spring = new JointSpring
         {
-            _controlKey = KeyCode.LeftArrow;
-            posMod = 1;
-        }
-        else _controlKey = KeyCode.RightArrow;
+            damper = Damper,
+            spring = HitForce
+        };
+        _joint = GetComponent<HingeJoint>();
+        _joint.spring = _spring;
+        _joint.useSpring = true;
+        JointLimits limits = _joint.limits;
+        limits.max = MaxAngle;
+        _joint.limits = limits;
+        _joint.useLimits = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(_controlKey))
+        if (Input.GetKeyDown(_controlKey))
         {
-            if(_currentRotation < MaxAngle)
-            {
-                _currentRotation += AngleStep;
-                if(_currentRotation > MaxAngle)
-                {
-                    float dif = _currentRotation - MaxAngle;
-                    _currentRotation -= dif;
-                    GetComponent<Transform>().Rotate(new Vector3(0, 0, 1), posMod * (AngleStep - dif), Space.World);
-                }
-                else GetComponent<Transform>().Rotate(new Vector3(0, 0, 1), posMod * AngleStep, Space.World);
-            }
+            _spring.targetPosition = MaxAngle;
+            _joint.spring = _spring;
         }
-        else
+        else if (Input.GetKeyUp(_controlKey))
         {
-            if (_currentRotation > 0f)
-            {
-                _currentRotation -= AngleStep;
-                if (_currentRotation < 0f)
-                {
-                    float dif = _currentRotation;
-                    _currentRotation -= dif;
-                    GetComponent<Transform>().Rotate(new Vector3(0, 0, 1), -1 * posMod * (AngleStep + dif), Space.World);
-                }
-                else GetComponent<Transform>().Rotate(new Vector3(0, 0, 1), posMod * -AngleStep, Space.World);
-            }
+            _spring.targetPosition = 0;
+            _joint.spring = _spring;
         }
     }
 }
