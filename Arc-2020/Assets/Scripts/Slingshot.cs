3d14fca584a702ed712c894f6bd1723e7dfbe1a4
jimuskin <jimuskin1234567890@gmail.com>
2020-05-19 21:59:36 +1000

    Added slingshots

---
+++ Arc-2020/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    /// <summary>
+    /// The velocity multiplier on hitting a slingshot
+    /// </summary>
+    [SerializeField] float velocityMultiplier = 4;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        GameObject ball = collision.collider.gameObject;
+        if (!ball.CompareTag("Ball")) return;
+
+        //Right direction is the red axis arrow (which is X in editor), so we take the transform.right of the collider.
+        Vector3 direction = transform.right;
+        Rigidbody ballRigidBody = ball.GetComponent<Rigidbody>();
+
+        ballRigidBody.velocity = direction * velocityMultiplier;
+
+    }
+}
