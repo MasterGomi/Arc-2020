3355c2af32702a5d3326891204bd5f33f83bf067
jimuskin <jimuskin1234567890@gmail.com>
2020-05-19 15:52:58 +1000

    Completed drop targets

---
+++ Arc-2020/Assets/Scripts/DropTargetComponent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTargetComponent : MonoBehaviour
+{
+
+    public bool Active { get; private set; }
+
+    private DropTargetManager targetManager;
+    private Collider targetCollider;
+
+
+    private void Start()
+    {
+        targetManager = transform.parent.GetComponent<DropTargetManager>();
+        targetCollider = GetComponent<Collider>();
+
+        Active = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject ball = other.gameObject;
+        if (!ball.CompareTag("Ball")) return;
+
+        Debug.Log("Hit a Drop Target");
+
+        if(Active)
+        {
+            DropTarget();
+        }
+
+
+    }
+
+    public void DropTarget()
+    {
+        Active = false;
+
+        targetCollider.enabled = false;
+        transform.localScale = new Vector3(0.6f, 0.02f, 0.6f);
+
+        if(targetManager != null)
+        {
+            targetManager.CheckForDroppedTargets();
+        }
+    }
+
+    public void ResetTarget()
+    {
+        targetCollider.enabled = true;
+        transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+
+        Active = true;
+    }
+}
---
+++ Arc-2020/Assets/Scripts/DropTargetManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTargetManager : MonoBehaviour
+{
+    /// <summary>
+    /// The list of drop targets which are grouped together by this manager.
+    /// </summary>
+    private DropTargetComponent[] dropTargets;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropTargets = GetComponentsInChildren<DropTargetComponent>();    
+    }
+
+    public void CheckForDroppedTargets()
+    {
+        //Check for all target components to see if they are active.
+        foreach(DropTargetComponent targetComponent in dropTargets)
+        {
+            if(targetComponent.Active == true)
+            {
+                return;
+            }
+        }
+
+        //Now that we know they are not active, set them all to active.
+        foreach (DropTargetComponent targetComponent in dropTargets)
+        {
+            targetComponent.ResetTarget();
+        }
+    }
+}
