04928054e6ede7524fa37b74610a98788a32b7a2
Grant Kearns <Kearnsgrantbusiness@gmail.com>
2020-05-19 15:02:20 +1000

    Ye this is grant's stuff yaba daba doo

---
+++ Arc-2020/Assets/Scripts/Bouncer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    public Rigidbody rb;
+    private float t = 0.0f;
+    private bool moving = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        UnityEngine.Debug.Log("hit , point value increase?");
+        rb.velocity = rb.velocity * 2.5f;
+        
+    }
+}
---
+++ Arc-2020/Assets/Scripts/Rollover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rollover : MonoBehaviour
+{
+    public Rigidbody rb;
+    private float t = 0.0f;
+    private bool moving = false;
+
+    void OnTriggerEnter(Collider other)
+    
+        {
+        UnityEngine.Debug.Log(this);
+        UnityEngine.Debug.Log("good");
+        var cubeRenderer = this.GetComponent<Renderer>();
+        cubeRenderer.material.SetColor("_Color", Color.black);
+    }
+}
