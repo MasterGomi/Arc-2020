da7d0ae387cd72163eee1e8fd596573279adb94c
Jackson Gray <jacksongray36@gmail.com>
2020-05-25 21:15:24 +1000

    Added more table elements, a rudimentery UI just for backend feedback, simple scoring, and modified table geometry slightly, mostly to line the orbit tracks up with the paddles a bit better

---
+++ Arc-2020/Assets/Scripts/ControlRollover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlRollover : Rollover, IController
+{
+    public EventNotify Notification;
+
+    private List<INotify> _subscribers = new List<INotify>();
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+        Notify(Notification);
+    }
+
+    public void Subscribe(INotify subscriber)
+    {
+        _subscribers.Add(subscriber);
+    }
+
+    public void Unsubscribe(INotify subscriber)
+    {
+        _subscribers.Remove(subscriber);
+    }
+
+    private void Notify(EventNotify notify)
+    {
+        foreach (INotify obj in _subscribers)
+        {
+            obj.Notify(notify);
+        }
+    }
+}
--- Arc-2020/Assets/Scripts/Rollover.cs
+++ Arc-2020/Assets/Scripts/Rollover.cs
@@ -1,40 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Timers;
 
-public class Rollover : MonoBehaviour, IController
+public class Rollover : MonoBehaviour
 {
+    /// <summary>
+    /// Score gained for triggering rollover
+    /// </summary>
     public int Score;
-    public EventNotify Notification;
+    /// <summary>
+    /// Minimum time (in seconds) between triggers (to prevent bugs where 
+    /// you might be able to score multiple time if collisions are registered in quick succession,
+    /// or if it's possible to roll back over the rollover and that shouldn't score)
+    /// </summary>
+    public float ResetTime;
 
-    private List<INotify> _subscribers = new List<INotify>();
+    private Timer _resetTimer;
+    private bool _canTrigger = true;
 
     private void Start()
     {
         if (Score > 0) TableManager.Manager.RegisterScores(this, Score);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (Score > 0) TableManager.Manager.Score(this);
-        Notify(Notification);
-    }
-
-    public void Subscribe(INotify subscriber)
-    {
-        _subscribers.Add(subscriber);
-    }
-
-    public void Unsubscribe(INotify subscriber)
-    {
-        _subscribers.Remove(subscriber);
-    }
-
-    private void Notify(EventNotify notify)
-    {
-        foreach(INotify obj in _subscribers)
+        _resetTimer = new Timer(ResetTime * 1000)
         {
-            obj.Notify(notify);
-        }
+            AutoReset = false,
+            Enabled = false,
+        };
+        _resetTimer.Elapsed += TriggerEnable;
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (!_canTrigger) return;
+
+        if (Score > 0) TableManager.Manager.Score(this);
+        _canTrigger = false;
+        _resetTimer.Start();
+    }
+
+    private void TriggerEnable(object source = null, ElapsedEventArgs e = null)
+    {
+        _canTrigger = true;
     }
 }
