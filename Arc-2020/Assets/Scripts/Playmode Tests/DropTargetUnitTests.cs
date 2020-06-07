a25f7654beb9f578837c89385839e01c26129165
jimuskin <jimuskin1234567890@gmail.com>
2020-06-07 23:48:18 +1000

    Initial Unit Test upload

---
+++ Arc-2020/Assets/Scripts/Playmode Tests/DropTargetUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class DropTargetUnitTests
+    {
+
+        //Checks to see if the drop targets are inactive after they've been dropped.
+        [Test]
+        public void DropTargetUnitTestsSimplePasses()
+        {
+            DropTargetComponent[] dropTargetComponents = GameObject.FindObjectsOfType<DropTargetComponent>();
+
+            foreach (DropTargetComponent dropTargetComponent in dropTargetComponents)
+            {
+                dropTargetComponent.DropTarget();
+                Assert.IsFalse(dropTargetComponent.Active);
+            }
+        }
+    }
+}
